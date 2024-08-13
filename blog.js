71f31be1f153a35297086dad4cfc552acd5a71d4
zhzhyu-com <mail@zhzhyu.com>
2024-08-13 18:02:00 +0800

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,45 @@
+
+/*羽毛_css*/
+.dropdown.wrapper {
+background:url(https://cdn.jsdelivr.net/gh/zimo6918/ZIMO6918-CDN/img/202202021631893.webp) right bottom no-repeat;
+}
+
+/*底部页脚*/
+.github-badge {
+  display: inline-block;
+  border-radius: 4px;
+  text-shadow: none;
+  font-size: 12px;
+  color: #fff;
+  line-height: 15px;
+  background-color: #abbac3;
+  margin-bottom: 5px
+}
+.github-badge .badge-subject {
+  display: inline-block;
+  background-color: #4d4d4d;
+  padding: 4px 4px 4px 6px;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px
+}
+.github-badge .badge-value {
+  display: inline-block;
+  padding: 4px 6px 4px 4px;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px
+}
+.github-badge .bg-blue {
+  background-color: #007ec6
+}
+.github-badge .bg-orange {
+  background-color: #ffa500
+}
+.github-badge .bg-red {
+  background-color: #f00
+}
+.github-badge .bg-green {
+  background-color: #3bca6e
+}
+.github-badge .bg-purple {
+  background-color: #ab34e9
+}
